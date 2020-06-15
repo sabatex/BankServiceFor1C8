4bf9f29c2b2b6b6dfa59c92691af24208d3bfb77
sabatex <serhiy.lakas@hotmail.com>
2020-06-15 10:03:06 +0300

    Add files via upload

---
+++ ResultConvert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankServiceFor1C8
+{
+    public class ResultConvert
+    {
+        public string FileName { get; set; }
+        public string Error { get; set; }
+        public string Result { get; set; }
+        public string FileUrl { get; set; }
+        public bool IsLoading { get; set; }
+    }
+}
