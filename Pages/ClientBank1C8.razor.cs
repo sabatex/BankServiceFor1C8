557aca27a2fa2754714bf648f8f499e2294fd93d
sabatex <serhiy.lakas@hotmail.com>
2021-02-18 09:55:35 +0200

    add support iFobs XML with zip

---
+++ Pages/ClientBank1C8.razor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.JSInterop;
+using sabatex.V1C8.BankHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankServiceFor1C8.Pages
+{
+    public partial class ClientBank1C8
+    {
+        async Task HandleSelection(InputFileChangeEventArgs files)
+        {
+            resultFiles.Clear();
+            error = "";
+            foreach (var f in files.GetMultipleFiles(3))
+            {
+                var result = new ResultConvert();
+
+                try
+                {
+                    using (MemoryStream memStream = new MemoryStream())
+                    {
+                        await f.OpenReadStream().CopyToAsync(memStream);
+                        string r = _1CClientBankExchange.ConvertTo1CFormat(bankType, memStream, accountNumber);
+                        result.FileName = "kb_to_1c" + f.Name + ".xml";
+                        result.FileUrl = await JSRuntime.InvokeAsync<string>("getFileUrl", r);
+                        resultFiles.Add(result);
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    error = error + e.Message;
+                }
+
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System.Globalization;
 using Microsoft.JSInterop;
+using MudBlazor.Services;
 
 namespace BankServiceFor1C8
 {
@@ -17,21 +18,23 @@ namespace BankServiceFor1C8
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.RootComponents.Add<App>("#app");
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMudServices();
             builder.Services.AddLocalization();
-           
-            var host = builder.Build();
-            var jsInterop = host.Services.GetRequiredService<IJSRuntime>();
-            var result = await jsInterop.InvokeAsync<string>("blazorCulture.get");
-            if (result != null)
-            {
-                var culture = new CultureInfo(result);
-                CultureInfo.DefaultThreadCurrentCulture = culture;
-                CultureInfo.DefaultThreadCurrentUICulture = culture;
-            }
 
-            await host.RunAsync();
+            //var host = builder.Build();
+            //var jsInterop = host.Services.GetRequiredService<IJSRuntime>();
+            //var result = await jsInterop.InvokeAsync<string>("blazorCulture.get");
+            //if (result != null)
+            //{
+            //    var culture = new CultureInfo(result);
+            //    CultureInfo.DefaultThreadCurrentCulture = culture;
+            //    CultureInfo.DefaultThreadCurrentUICulture = culture;
+            //}
+
+            //await host.RunAsync();
+            await builder.Build().RunAsync();
        }
     }
 }
