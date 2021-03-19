d4817960d62bc0979f4b7f88a8e12a1ec7950dd5
Сергей Лакас <serhiy.lakas@hotmail.com>
2021-03-19 15:00:05 +0200

    MudBlazor package update, remove IFobs dublicate

--- Pages/ClientBank1C8.razor.cs
+++ Pages/ClientBank1C8.razor.cs
@@ -23,8 +23,8 @@ namespace BankServiceFor1C8.Pages
         private EBankType bankType = EBankType.iBankUA_TXT;
         private static Dictionary<EBankType, string> stringBankPresents = new Dictionary<EBankType, string>
         {
-            {EBankType.iFobsUA_XML, "iFobs формат файла XML (ZIP)"},
-            {EBankType.iFobsUA_TXT, "iFobs формат файла TXT (DAT)" },
+            //{EBankType.iFobsUA_XML, "iFobs формат файла XML (ZIP)"},
+            //{EBankType.iFobsUA_TXT, "iFobs формат файла TXT (DAT)" },
             {EBankType.iFobs,"iFobs формат вивантаження XML/TXT (XML,DAT,ZIP)" },
             {EBankType.iBankUA_TXT, "iBank формат файла ТХТ (csv)"},
             {EBankType.PrivatUA,"ПриватБанк формат файла ТХТ (csv)"},
